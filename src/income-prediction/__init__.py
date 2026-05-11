335b52440f78c5e0b7f097d7f53e53dd850a507a
Robert Abel <robert.abel@cariad.technology>
2026-05-11 17:33:17 +0200

    add W&B Artifact work-around for Windows
    Add work-around for broken W&B Artifact.file API on NTFS (Windows).
    W&B tries to create `<artifact-name>:<alias>`, where colon (:) is
    a forbidden character on NTFS.
    
    Use the approach used by other APIs such as Artifact.download and
    replace with dash (-) for `<artifact-name>-<alias>` instead.
    
    See wandb#1143 [1] for similar bug report.
    
    Signed-off-by: Robert Abel <robert.abel@cariad.technology>
    
        [1]: https://github.com/wandb/wandb/issues/1143

--- src/income-prediction/__init__.py
+++ src/income-prediction/__init__.py
@@ -1,2 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+
+from typing import TYPE_CHECKING
+
+import wandb
+
+if TYPE_CHECKING:
+    from wandb.sdk.lib.paths import StrPath
+
+
+wandb_artifact_file_fn = wandb.Artifact.file
+
+
+def wandb_artifact_file_fix(self: 'wandb.Artifact', root: str | None = None) -> 'StrPath':
+    """Fix for file systems where colon (:) cannot be used in file names, such as NTFS on Windows.
+
+    Args:
+        self: W&B Artifact instance
+        root: Optional root directory to use for the file path.
+    """
+    root = str(root or self._default_root())
+    return wandb_artifact_file_fn(self, root)
+
+
+wandb.Artifact.file = wandb_artifact_file_fix
