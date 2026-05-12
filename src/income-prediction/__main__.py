a7c81a61e6cc0ceee928ed3cef0954ff85b765df
Robert Abel <robert.abel@cariad.technology>
2026-05-12 18:19:35 +0200

    implement model class and training
    Refactor starter code into dedicated model class. Incorporate data preparation
    and running the training loop into main. Use dvc and W&B to track experiments.
    
    Signed-off-by: Robert Abel <robert.abel@cariad.technology>

--- src/income-prediction/__main__.py
+++ src/income-prediction/__main__.py
@@ -2,10 +2,167 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import logging
 import os
 import sys
 
 from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import yaml
+import wandb
+from sklearn.model_selection import train_test_split
+
+from model import IncomePredictionModel
+from __init__ import *  # noqa: F401,F403
+
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO,
+                    format='[%(asctime)s][%(levelname)-8s] %(message)s', datefmt='%d %b %Y %H:%M:%S')
+logger = logging.getLogger(__package__)
+
+
+def prepare_data(config_path: Path) -> None:
+    """Split and Store Prepared Data for Training and Testing
+
+    Args:
+        config_path: path to the preprocessing configuration file
+
+    Raises:
+        OSError: if configuration or output files cannot be accessed
+        ValueError: if the configuration file is missing mandatory keys
+    """
+    logger.info(f'Preparing the training and test datasets from {config_path}...')
+
+    # read configuration
+    with open(config_path, 'r') as f:
+        config = yaml.safe_load(f)
+
+    # check configuration
+    mandatory_keys = ('data', 'split', 'random-seed', 'test-size', 'stratify')
+    missing_keys = tuple(k for k in mandatory_keys if k not in config)
+    if missing_keys:
+        raise ValueError(f'Missing mandatory keys in the configuration: {", ".join(missing_keys)}')
+
+    # seed random number generator
+    # https://scikit-learn.org/stable/glossary.html#term-random_state
+    np.random.seed(config['random-seed'])
+
+    # load dataset
+    run = wandb.init(job_type='train-test-split')
+    filename = run.use_artifact(config['data'], type='cleaned-data').file()
+    data = pd.read_csv(filename)
+
+    # store config
+    run.config.update(config)
+
+    train: pd.DataFrame
+    test: pd.DataFrame
+    stratify_df = data[config['stratify']] if config['stratify'] else None
+    train, test = train_test_split(data, test_size=config['test-size'], stratify=stratify_df)
+
+    # create metrics
+    metrics = {
+        'n_total': len(train) + len(test),
+        'n_test': len(test),
+        'n_train': len(train),
+    }
+
+    # add metrics to summary
+    run.summary.update(metrics)
+
+    # create output directory
+    outdir = Path(__file__).parent / 'data'
+    outdir.mkdir(parents=True, exist_ok=True)
+
+    # store metrics
+    with open(outdir / 'metrics.yml', 'w') as f:
+        yaml.safe_dump(metrics, f)
+
+    # store dataset artifact
+    train.to_csv(outdir / 'train.csv', index=False)
+    test.to_csv(outdir / 'test.csv', index=False)
+
+    artifact = wandb.Artifact(config['split'], type='cleaned-data',
+                              description='Split Train/Test Dataset')
+    artifact.add_file(outdir / 'train.csv')
+    artifact.add_file(outdir / 'test.csv')
+    run.log_artifact(artifact)
+    artifact.wait()
+
+    # finish the run
+    run.finish()
+
+
+def train_model(config_path: Path) -> None:
+    """Train and Store the Income Prediction Model from Prepared Data
+
+    Args:
+        config_path: path to the training configuration file
+
+    Raises:
+        OSError: if configuration, metrics output, or training data files cannot be accessed
+        ValueError: if the configuration file is missing mandatory keys
+    """
+    logger.info(f'Training the model with configuration from {config_path}...')
+
+    # read configuration
+    with open(config_path, 'r') as f:
+        config = yaml.safe_load(f)
+
+    # check configuration
+    mandatory_keys = ('data', 'model', 'random-seed', 'random-forest-classifier')
+    missing_keys = tuple(k for k in mandatory_keys if k not in config)
+    if missing_keys:
+        raise ValueError(f'Missing mandatory keys in the configuration: {", ".join(missing_keys)}')
+
+    # seed random number generator
+    # https://scikit-learn.org/stable/glossary.html#term-random_state
+    np.random.seed(config['random-seed'])
+
+    # load training data
+    run = wandb.init(job_type='training')
+    dirname = run.use_artifact(config['data'], type='cleaned-data').download()
+    train = pd.read_csv(Path(dirname) / 'train.csv')
+    test = pd.read_csv(Path(dirname) / 'test.csv')
+
+    # store config
+    run.config.update(config)
+
+    # actually train the model
+    model = IncomePredictionModel.from_config(config['random-forest-classifier'])
+    model.train(train)
+    precision, recall, f_beta = model.validate(test)
+
+    # create metrics
+    metrics = {
+        'precision': precision,
+        'recall': recall,
+        'f-beta': f_beta
+    }
+
+    # add metrics to summary
+    run.summary.update(metrics)
+
+    # create output directory
+    outdir = Path(__file__).parent / 'model'
+    outdir.mkdir(parents=True, exist_ok=True)
+
+    # store metrics
+    with open(outdir / 'metrics.yml', 'w') as f:
+        yaml.safe_dump(metrics, f)
+
+    # store model artifact
+    model.store_model(outdir / 'model.joblib')
+    artifact = wandb.Artifact(config['model'], type='model',
+                              description='Trained Pipeline for Income Prediction')
+    artifact.add_file(outdir / 'model.joblib')
+    run.log_artifact(artifact)
+    artifact.wait()
+
+    # finish the run
+    run.finish()
 
 
 def main() -> int:
@@ -13,14 +170,32 @@ def main() -> int:
     subparsers = parser.add_subparsers(title='command', dest='command', description='Sub-Command')
     parser.set_defaults(command='serve')
 
-    serve_parser = subparsers.add_parser('serve', help='Serve the App (default)')
+    serve_parser = subparsers.add_parser('serve', help='Serve the App (default)')  # noqa: F841
+
+    prep_parser = subparsers.add_parser('prep', help='Prepare the Data')
+    prep_parser.add_argument('--config', type=Path, required=True, help='Path to the preprocessing configuration file')
 
     train_parser = subparsers.add_parser('train', help='Train the Model')
     train_parser.add_argument('--config', type=Path, required=True, help='Path to the training configuration file')
 
     args = parser.parse_args()
 
-    print(f'Args: {args}')
+    # abort with fatal error message in case WANDB_PROJECT is not set
+    if 'WANDB_PROJECT' not in os.environ:
+        logger.critical('WANDB_PROJECT environment variable must be set!')
+        return 1
+
+    match (args.command):
+        case 'prep':
+            prepare_data(args.config)
+        case 'train':
+            train_model(args.config)
+        case 'serve':
+            # TODO: implement web server + FastAPI app
+            raise NotImplementedError('Serve command is not implemented yet')
+            pass
+        case _:
+            raise NotImplementedError(f'Unknown command: {args.command}')
 
     return 0
 
--- src/income-prediction/model.py
+++ src/income-prediction/model.py
@@ -1,58 +1,208 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__all__ = (
+    'IncomePredictionModel',
+)
+
+import logging
+import sys
+from typing import Any, Self, TYPE_CHECKING
+
+import numpy as np
+import joblib
+import sklearn
+
+from sklearn.compose import ColumnTransformer, make_column_selector
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.impute import SimpleImputer
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.preprocessing import OneHotEncoder
+
+if TYPE_CHECKING:
+    import os
+
+    import numpy.typing as npt
+    import pandas as pd
+
+logger = logging.getLogger(__name__)
 
 
-def train_model(X_train, y_train):
-    """
-    Trains a machine learning model and returns it.
+class IncomePredictionModel:
 
-    Inputs
-    ------
-    X_train : np.ndarray
-        Training data.
-    y_train : np.ndarray
-        Labels.
-    Returns
-    -------
-    model : RandomForestClassifier
-        Trained machine learning model.
-    """
-    pass
+    __create_key = object()
+    """Token to prevent direct instantiation"""
 
+    def __init__(self, pipeline: Pipeline, *, key: object):
+        """Create an Income Prediction Model
 
-def compute_model_metrics(y, preds):
-    """
-    Validates the trained machine learning model using precision, recall, and F1.
+        Private, use the from_config()/from_model() methods to create an instance.
+        """
+        if key is not IncomePredictionModel.__create_key:
+            raise ValueError("Use the from_config()/from_model() methods to create an instance.")
 
-    Inputs
-    ------
-    y : np.ndarray
-        Known labels, binarized.
-    preds : np.ndarray
-        Predicted labels, binarized.
-    Returns
-    -------
-    precision : float
-    recall : float
-    fbeta : float
-    """
-    fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
-    precision = precision_score(y, preds, zero_division=1)
-    recall = recall_score(y, preds, zero_division=1)
-    return precision, recall, fbeta
+        self._pipeline = pipeline
 
+    @classmethod
+    def from_config(cls, config: dict[str, Any]) -> 'Self':
+        """Create an IncomePredictionModel from a configuration dictionary
+           ready to be trained.
 
-def inference(model, X):
-    """ Run model inferences and return the predictions.
+        Args:
+            config: A dictionary containing the configuration parameters for Random Forest Classifier.
 
-    Inputs
-    ------
-    model : RandomForestClassifier
-        Trained machine learning model.
-    X : np.ndarray
-        Data used for prediction.
-    Returns
-    -------
-    preds : np.ndarray
-        Predictions from the model.
-    """
-    pass
+        Returns:
+            An instance of IncomePredictionModel initialized with the given configuration.
+        """
+        # transform numeric data and impute with zeros
+        # frontend needs to deal with mandatory values
+        numeric_xform = make_pipeline(
+            SimpleImputer(strategy='constant', fill_value=0)
+        )
+        # transform categorical data and impute missing values with
+        category_xform = make_pipeline(
+            SimpleImputer(strategy='constant', fill_value='<Unknown>'),
+            OneHotEncoder(sparse_output=False, handle_unknown='ignore')
+        )
+
+        # create the column transformer
+        col_xform = ColumnTransformer(
+            transformers=[
+                ('numeric',     numeric_xform, make_column_selector(dtype_include=np.number)),
+                ('categorical', category_xform, make_column_selector(dtype_include=['object', 'string'])),
+            ],
+            # drop untransformed columns
+            remainder='drop'
+        )
+
+        # create the pipeline
+        pipeline = Pipeline(
+            steps=[
+                ('preprocessor', col_xform),
+                ('classifier', RandomForestClassifier(**config))
+            ]
+        )
+
+        # construct the model with out skeleton pipeline
+        return cls(pipeline, key=cls.__create_key)
+
+    @classmethod
+    def from_model(cls, filename: 'str | os.PathLike[str]') -> 'Self':
+        """Create an IncomePredictionModel from a pre-trained model.
+
+        Args:
+            filename: path to the model file
+
+        Returns:
+            An instance of IncomePredictionModel initialized with the given model.
+        """
+        model = joblib.load(filename)
+
+        # make sure it's a dictionary
+        if not isinstance(model, dict):
+            raise ValueError(f"Expected model file to contain a dictionary, got {type(model)}")
+
+        # make sure mandatory keys are present
+        keys = ('pipeline', 'sklearn-version', 'python-version')
+        missing_keys = tuple(x for x in keys if x not in model)
+        if missing_keys:
+            raise ValueError(
+                f'Expected model file to contain keys {", ".join(keys)}, but missing key(s) {", ".join(missing_keys)}'
+            )
+
+        logger.info(
+            f'Loaded model from {filename}: scikit-learn {model["sklearn-version"]} (Python {model["python-version"]})'
+        )
+
+        # construct the model from a fixed pre-trained pipeline
+        return cls(model['pipeline'], key=cls.__create_key)
+
+    def store_model(self, filename: 'str | os.PathLike[str]'):
+        """Store the current IncomePredictionModel to a file.
+
+        Args:
+            filename: path to the model file
+        """
+        model = {
+            'pipeline': self._pipeline,
+            'sklearn-version': sklearn.__version__,
+            'python-version': f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}',
+        }
+        joblib.dump(model, filename)
+        logger.info(f'Model stored to {filename}')
+
+    @staticmethod
+    def _prepare_data(
+        data: 'pd.DataFrame',
+        label: str | None = None,
+        weights: str | None = None,
+    ) -> tuple['pd.DataFrame', 'pd.Series', 'pd.Series']:
+        """Prepare the dataset for training or inference.
+
+        Args:
+            data: Dataset to prepare
+            label: Column name for the target variable
+            weights: Column name for the sample weights
+
+        Returns:
+            Tuple (X, y, w) containing the prepared features, labels, and sample weights.
+        """
+        X = data.drop(columns=[label, weights], errors='ignore')
+        y = data[label] if label else None
+        w = data[weights] if weights else None
+        return X, y, w
+
+    def train(self, train: 'pd.DataFrame'):
+        """Train the Random Forest Classifier
+
+        Trains a machine learning model and saves it to the `model` directory.
+
+        Args:
+            train: Training Dataset
+        """
+        X_train, y_train, w_train = IncomePredictionModel._prepare_data(train, label='salary', weights='fnlwgt')
+
+        logger.info(f'Training model on {len(X_train)} samples...')
+        self._pipeline.fit(X_train, y_train, classifier__sample_weight=w_train)
+
+    def validate(self, test: 'pd.DataFrame') -> tuple[float, float, float]:
+        """Validates the trained machine learning model using precision, recall, and F1.
+
+        Args:
+            test: Test Dataset
+
+        Returns:
+            A tuple containing the precision, recall, and F1 score of the trained model on the test set.
+        """
+        X, y, w = IncomePredictionModel._prepare_data(test, label='salary', weights='fnlwgt')
+        # calculate predictions
+        preds = self._pipeline.predict(X)
+        # keyword-arguments for score functions
+        kwargs = {
+            # scikit-learn stores labels in lexicographic ascending order, so outputs will
+            # be stable for same class labels ('<=50K' will sort before '>50K').
+            # See https://scikit-learn.org/stable/glossary.html#term-classes_
+            # Therefore, use top-most classifier label as positive class
+            'pos_label': self._pipeline.named_steps['classifier'].classes_[-1],
+            'sample_weight': w,
+            # treat zero-division as perfect score
+            'zero_division': 1,
+        }
+        # calculate weighed metrics against known labels
+        fbeta = fbeta_score(y, preds, beta=1, **kwargs)
+        precision = precision_score(y, preds, **kwargs)
+        recall = recall_score(y, preds, **kwargs)
+        return precision, recall, fbeta
+
+    def predict(self, data: 'pd.DataFrame') -> 'npt.NDArray[np.str_]':
+        """ Run model inferences and return the predictions.
+
+        Args:
+            data: Dataframe containing the features to run inference on.
+
+        Returns:
+            A numpy array containing the predicted labels.
+        """
+        X, _, _ = IncomePredictionModel._prepare_data(data, label='salary', weights='fnlwgt')
+        return self._pipeline.predict(X)
